9991fd26a755f50dad67799ace2ed9e991b6488d
Matthias Blume <blume@google.com>
2023-12-04 14:24:34 -0600

    Even more tuple structs.

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,26 +1,28 @@
 use std::env;
 use std::fs;
 
-fn read_color(input: &str) -> (u32, u32, u32) {
+struct Triplet(u32, u32, u32);
+
+fn read_color(input: &str) -> Triplet {
     let v: Vec<&str> = input.split_whitespace().collect();
     let value: u32 = v[0].parse().unwrap();
     match v[1] {
-        "red" => (value, 0, 0),
-        "green" => (0, value, 0),
-        "blue" => (0, 0, value),
-        _ => (0, 0, 0),
+        "red" => Triplet(value, 0, 0),
+        "green" => Triplet(0, value, 0),
+        "blue" => Triplet(0, 0, value),
+        _ => Triplet(0, 0, 0),
     }
 }
 
-fn add_triplet((xr, xg, xb): (u32, u32, u32), (yr, yg, yb): (u32, u32, u32)) -> (u32, u32, u32) {
-    (xr + yr, xg + yg, xb + yb)
+fn add_triplet(Triplet(xr, xg, xb): Triplet, Triplet(yr, yg, yb): Triplet) -> Triplet {
+    Triplet(xr + yr, xg + yg, xb + yb)
 }
 
-fn read_triplet(input: &str) -> (u32, u32, u32) {
-    input.split(",").map(read_color).fold((0, 0, 0), add_triplet)
+fn read_triplet(input: &str) -> Triplet {
+    input.split(",").map(read_color).fold(Triplet(0, 0, 0), add_triplet)
 }
 
-fn read_triplets(input: &str) -> Vec<(u32, u32, u32)> {
+fn read_triplets(input: &str) -> Vec<Triplet> {
     input.split(";").map(read_triplet).collect()
 }
 
@@ -32,20 +34,20 @@ fn read_game_number(input: &str) -> u32 {
 }
 
 
-fn read_game(line: &str) -> (u32, Vec<(u32, u32, u32)>) {
+fn read_game(line: &str) -> (u32, Vec<Triplet>) {
     let v: Vec<&str> = line.split(":").collect();
     (read_game_number(v[0]), read_triplets(v[1]))
 }
 
-fn triplet_possible(red: u32, green: u32, blue: u32, (r, g, b): &(u32, u32, u32)) -> bool {
+fn triplet_possible(red: u32, green: u32, blue: u32, Triplet(r, g, b): &Triplet) -> bool {
     *r <= red && *g <= green && *b <= blue
 }
 
-fn minimum_triplet_power(triplets: &Vec<(u32, u32, u32)>) -> u32 {
+fn minimum_triplet_power(triplets: &Vec<Triplet>) -> u32 {
     let mut min_red: u32 = 0;
     let mut min_green: u32 = 0;
     let mut min_blue: u32 = 0;
-    for (r, g, b) in triplets.iter() {
+    for Triplet(r, g, b) in triplets.iter() {
         if *r > min_red { min_red = *r }
         if *g > min_green { min_green = *g }
         if *b > min_blue { min_blue = *b }
