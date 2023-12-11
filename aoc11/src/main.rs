2e0796d2926954052600e44fc1680f15bea216e3
Matthias Blume <blume@google.com>
2023-12-11 13:38:44 -0600

    Various improvements to Day 11 code.
    (Still room left, though.)

--- aoc11/src/main.rs
+++ aoc11/src/main.rs
@@ -3,61 +3,49 @@ use std::fs;
 
 type Pos = (usize, usize);
 
-fn read_unexpanded_line(r: usize, line: &str, set: &mut Vec<Pos>) -> bool {
-    let mut c = 0;
-    let mut non_empty = false;
-    for ch in line.chars() {
-        match ch {
-            '.' => (),
-            '#' => { set.push((r, c)); non_empty = true },
-            _ => panic!("bad space"),
-        }
-        c += 1
-    }
-    non_empty
+fn read_galaxies(contents: &str) -> Vec<Pos> {
+    contents
+        .lines()
+        .enumerate()
+        .flat_map(|(r, line)|
+                  line.chars().enumerate()
+                  .filter_map(move |(c, ch)|
+                              match ch {
+                                  '.' => None,
+                                  '#' => Some((r, c)),
+                                  _ => panic!("bad space"),
+                              }))
+        .collect()
 }
 
-fn read_unexpanded_lines(contents: &str, set: &mut Vec<Pos>) {
-    let mut r = 0;
-    for line in contents.lines() {
-        if read_unexpanded_line(r, line, set) {
-            r += 1;
-        } else {
-            r += 1000000;
-        }
-    }
+fn compute_stretch<'a>(
+    factor: usize, galaxies: &'a Vec<Pos>, select: impl Fn(&'a Pos) -> usize)
+    -> Vec<usize> {
+    let len = 1 + galaxies.iter().map(&select).max().unwrap_or(0);
+    (0..len).scan(0, |stretch, i| {
+        let s = *stretch;
+        let is_empty = galaxies.iter().all(|p| select(p) != i);
+        if is_empty { *stretch += factor-1 }
+        Some(s)
+    }).collect()
 }
 
-fn col_is_empty(col: usize, set: &Vec<Pos>) -> bool {
-    set.iter().all(|&(_, c)| c != col)
+fn expand(galaxies: Vec<Pos>, stretch_factor: usize) -> Vec<Pos> {
+    let row_stretch = compute_stretch(stretch_factor, &galaxies, |&(r, _)| r);
+    let col_stretch = compute_stretch(stretch_factor, &galaxies, |&(_, c)| c);
+    galaxies.iter().map(|&(r, c)| (r + row_stretch[r], c + col_stretch[c])).collect()
 }
 
-fn col_stretch(ncols: usize, set: &Vec<Pos>) -> Vec<usize> {
-    let mut stretch = 0;
-    let mut v = Vec::new();
-    for c in 0..ncols {
-        v.push(stretch);
-        if col_is_empty(c, set) {
-            stretch += 999999
-        }
-    }   
-    v
-}
-
-fn expand(unexpanded: Vec<Pos>) -> Vec<Pos> {
-    let ncols = unexpanded.iter().fold(0, |nc, &(_, c)| if c > nc { c } else { nc }) + 1;
-    let stretch = col_stretch(ncols, &unexpanded);
-    unexpanded.iter().map(|&(r, c)| (r, c + stretch[c])).collect()
-}
-
-fn manhattan_distance(x: Pos, y: Pos) -> usize {
-    (if x.0 < y.0 { y.0 - x.0 } else { x.0 - y.0 })
-        + (if x.1 < y.1 { y.1 - x.1 } else { x.1 - y.1 })
+fn distance(x: Pos, y: Pos) -> usize {
+    x.0.abs_diff(y.0) + x.1.abs_diff(y.1)
 }
 
 fn total_distance(expanded: &Vec<Pos>) -> usize {
     let len = expanded.len();
-    (0..len).map(|i| (i+1..len).map(|j| manhattan_distance(expanded[i], expanded[j])).sum::<usize>()).sum()
+    (0..len)
+        .fold(0, |isum, i|
+              (i+1..len).fold(isum, |jsum, j|
+                              jsum + distance(expanded[i], expanded[j])))
 }
 
 fn main() {
@@ -70,14 +58,19 @@ fn main() {
         Some(arg) => arg,
         _ => panic!("{}: no program name", program),
     };
+    let stretch_factor: usize =
+        if let Some(arg) = args.next() {
+            arg.parse().expect("stretch factor")
+        } else {
+            2
+        };
 
     let contents = fs::read_to_string(file_path)
         .expect("Could not read file");
 
-    let mut unexpanded = Vec::new();
-    read_unexpanded_lines(&contents, &mut unexpanded);
-    let expanded = expand(unexpanded);
-    let total = total_distance(&expanded);
-    
+    let galaxies = read_galaxies(&contents);
+    let expanded_galaxies = expand(galaxies, stretch_factor);
+    let total = total_distance(&expanded_galaxies);
+
     println!("{total}")
 }
