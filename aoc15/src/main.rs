299010188a66c5c08ad4ebd9f6bf604af8e0e5ff
Matthias Blume <blume@google.com>
2023-12-15 11:19:39 -0600

    Cleanup for Day 15.
    Various minor and cosmetic code improvements.

--- aoc15/src/main.rs
+++ aoc15/src/main.rs
@@ -1,70 +1,81 @@
 use std::env;
 use std::fs;
 
-fn ascii_hash(s: &str) -> u64 {
-    s.as_bytes().iter().fold(0, |accu, &c| ((accu + c as u64) * 17) % 256)
+struct Lens {
+    label: String,
+    strength: u8,
 }
 
-fn line_total(line: &str) -> u64 {
+struct Box{
+    n: u8,  // box number
+    lenses: Vec<Lens>,
+}
+
+struct Boxes(Vec<Box>);
+
+fn ascii_hash(s: &str) -> usize {
+    s.as_bytes().iter().fold(0, |accu, &c| ((accu + c as usize) * 17) % 256)
+}
+
+fn line_total(line: &str) -> usize {
     line.split(",").map(ascii_hash).sum()
 }
 
-struct Lens(String, u8);   // label, strength
+impl Box {
+    fn lens_position(self: &Self, label: &str) -> Option<usize> {
+        self.lenses.iter().position(|lens| lens.label == label)
+    }
 
-struct Box(u8, Vec<Lens>);  // boxnum, lenses
-
-type Boxes = Vec<Box>;
-
-fn remove_lens(label: &str, b: &mut Box) {
-    for i in 0..b.1.len() {
-        if b.1[i].0 == label {
-            b.1.remove(i);
-            return
+    fn remove_lens(self: &mut Self, label: &str) {
+        if let Some(i) = self.lens_position(label) {
+            self.lenses.remove(i);
         }
     }
-}
 
-fn insert_lens(label: &str, strength: u8, b: &mut Box) {
-    let l = Lens(label.to_string(), strength);
-    for i in 0..b.1.len() {
-        if b.1[i].0 == label {
-            b.1[i] = l;
-            return;
+    fn insert_lens(self: &mut Self, label: &str, strength: u8) {
+        let lens = Lens{ label: label.to_string(), strength };
+        if let Some(i) = self.lens_position(label) {
+            self.lenses[i] = lens;
+        } else {
+            self.lenses.push(lens);
         }
     }
-    b.1.push(l)
+
+    fn strength(self: &Self) -> usize {
+        (self.n as usize + 1) *
+            self.lenses.iter().enumerate()
+                .map(|(position, lens)|
+                     (position + 1) * (lens.strength as usize))
+                .sum::<usize>()
+    }
 }
 
-fn apply_instruction(ins: &str, boxes: &mut Boxes) {
-    match ins.split("=").collect::<Vec<_>>().as_slice() {
-        [label, strength] => {
-            let h = ascii_hash(label);
-            let s = strength.parse::<u8>().unwrap();
-            insert_lens(label, s, &mut boxes[h as usize])
-        },
-        _ => {
-            if ins.chars().last().unwrap() != '-' { panic!("bad instruction") };
-            let label = &ins[..ins.len()-1];
-            let h = ascii_hash(&label);
-            remove_lens(label, &mut boxes[h as usize]);
+impl Boxes {
+    fn apply_instruction(self: &mut Self, ins: &str) {
+        let Boxes(ref mut boxes) = self;
+        match ins.split("=").collect::<Vec<_>>().as_slice() {
+            [label, strength] => {
+                let h = ascii_hash(label);
+                let s = strength.parse::<u8>().expect("lens strength");
+                boxes[h].insert_lens(label, s)
+            },
+            _ => {
+                if ins.chars().last() != Some('-') { panic!("bad instruction") };
+                let label = &ins[..ins.len()-1];
+                let h = ascii_hash(&label);
+                boxes[h].remove_lens(label);
+            }
         }
     }
-}
 
-fn apply_line(line: &str, boxes: &mut Boxes) {
-    for ins in line.split(",") {
-        apply_instruction(ins, boxes);
+    fn apply_line(self: &mut Self, line: &str) {
+        line.split(",").for_each(|ins| self.apply_instruction(ins))
     }
-}
 
-fn box_strength(b: &Box) -> usize {
-    b.1.iter().enumerate()
-        .map(|(i, &Lens(_, s))| (b.0 as usize + 1) * (i + 1) * (s as usize))
-        .sum()
-}
-
-fn total_strength(boxes: &Boxes) -> usize {
-    boxes.iter().map(box_strength).sum()
+    fn strength(self: &Self) -> usize {
+        let Boxes(boxes) = self;
+        boxes.iter().map(|b| b.strength()).sum()
+    }
 }
 
 fn main() {
@@ -81,19 +92,17 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Could not read file");
 
-    let mut boxes: Boxes = Vec::new();
-    for i in 0..=255 {
-        boxes.push(Box(i, Vec::new()));
-    }
+    let mut boxes: Boxes =
+        Boxes((0..=255).map(|n| Box{ n, lenses: Vec::new() }).collect());
 
     let mut part1_total = 0;
     
     for line in contents.lines() {
         part1_total += line_total(line);
-        apply_line(line, &mut boxes);
+        boxes.apply_line(line);
     }
 
-    let part2_total = total_strength(&boxes);
+    let part2_total = boxes.strength();
     
     println!("part 1: {part1_total}, part 2: {part2_total}")
 }
